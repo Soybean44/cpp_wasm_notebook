0444abe1787186e4211ac9f7b4a25d1750c430c3
Sovereign Shahid <sovereignshahid@gmail.com>
2025-04-22 14:14:52 -0600

    Created documentation for the project

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # cpp_wasm_notebook
+
 A test repository to see If i could compile a C++ quarto notebook to wasm and run it as a website
+
+## Documentation
+
+To create a notebook like the one described here all you need is a qmd file which is being compiled that contains cpp and includes some html/js which runs the generated wasm file.
+To start, just copy over the scripts file and make sure you use the in `_quarto.yml` you have the following line
+
+```yml
+project:
+  pre-render: scripts/render_cpp.py
+  resources: "*.wasm"
+```
+
+This ensures that the wasm is copied over to the final rendered project as well as compiles down the cpp codeblocks to wasm
+
+From there look to the `cpp` directory to get an idea of how to structure a wasm notebook by the given DVD bounce example. Note that this is raw wasm from clang, so you have to interface with the javascript on your own, as well as remembering that there is no standard library, however if need be you can modify the script to link with [newlibc](https://github.com/bminor/newlib).
--- scripts/render_cpp.py
+++ scripts/render_cpp.py
@@ -6,11 +6,39 @@ Description: This file is used as a pre-render script to extract cpp codeblocks 
              and compile them to wasm for use in quarto web pages.
 """
 
+import ctypes
 import os
+import shutil
 import subprocess
 import sys
 
 import exdown
+
+# --- CONSTANTS --- #
+CFLAGS = [
+    "-Wall",
+    "--target=wasm32",
+    "-I./scripts/includes/",  # this includes the wasm.h file
+    "-Os",
+    "-flto",
+    "--no-standard-libraries",
+    "-fvisibility=hidden",
+    "-std=c++14",
+    "-ffunction-sections",  # This makes the function names normal
+    "-fdata-sections",
+]
+
+LDFLAGS = [
+    "--no-entry",
+    "--strip-all",
+    "--export-dynamic",
+    "--allow-undefined",  # This allows for external definitions in js
+    "--initial-memory=131072",
+    "--error-limit=0",
+    "--lto-O3",
+    "-O3",
+    "--gc-sections",
+]
 
 file_str = os.getenv("QUARTO_PROJECT_INPUT_FILES")
 if file_str is None:
@@ -29,6 +57,7 @@ def has_cpp(filename: str) -> bool:
     return False
 
 
+# Get all the files to be compiled to
 cpp_files = [file for file in files if has_cpp(file)]
 
 if cpp_files == []:
@@ -37,43 +66,30 @@ if cpp_files == []:
 
 
 def compile_cpp(filepath: str) -> None:
-    command = [
-        "clang",
-        "-Wall",
-        "--target=wasm32",
-        "-I./scripts/includes/",
-        "-Os",
-        "-flto",
-        "--no-standard-libraries",
-        "-fvisibility=hidden",
-        "-std=c++14",
-        "-ffunction-sections",
-        "-fdata-sections",
-        "-Wl,--no-entry",
-        "-Wl,--strip-all",
-        "-Wl,--export-dynamic",
-        "-Wl,--allow-undefined",
-        "-Wl,--initial-memory=131072",
-        "-Wl,--error-limit=0",
-        "-Wl,--lto-O3",
-        "-Wl,-O3",
-        "-Wl,--gc-sections",
-        "-o",
-        f"{filepath}.wasm",
-        f"{filepath}.cpp",
-    ]
-    print(" ".join(command))
+    command = (
+        ["clang"]
+        + CFLAGS
+        + [f"-Wl,{flag}" for flag in LDFLAGS]
+        + [
+            "-o",
+            f"{filepath}.wasm",
+            f"{filepath}.cpp",
+        ]
+    )
     subprocess.run(command, check=True)
 
 
 for file in cpp_files:
     print(f"Parsing: {file}")
-    cpp_code = "\n".join(line[0] for line in exdown.extract(file, syntax_filter="cpp"))
-    if (filename := os.path.basename(file)) is not None:
-        filename = filename.split(".")[0]
-    directory = os.path.dirname(file)
-    filepath = f"{directory}/{filename}" if directory != "" else filename
-    with open(f"{filepath}.cpp", "w", encoding="UTF-8") as f:
-        f.write(cpp_code)
-    # TODO: Implement compilation to wasm
-    compile_cpp(filepath)
+
+    # check if the file exists and get the filename
+    if file is not None:
+        # Extract cpp code from qmd codeblocks
+        cpp_code = "\n".join(
+            line[0] for line in exdown.extract(file, syntax_filter="cpp")
+        )
+        filename = file.split(".")[0]
+
+        with open(f"{filename}.cpp", "w", encoding="UTF-8") as f:
+            f.write(cpp_code)
+        compile_cpp(filename)
